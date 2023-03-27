6d44bbc232d67e417db8a5a402f01e81766961ba
An4Chi <kroliktha@gmail.com>
2023-03-27 14:38:38 -0500

    Save

--- src/test/java/com/library/steps/US04_StepDef.java
+++ src/test/java/com/library/steps/US04_StepDef.java
@@ -12,25 +12,45 @@ import java.util.Map;
 
 public class US04_StepDef extends BookPage{
 
+    String classBookName;
+
     @Given("the user navigates to {string} page")
     public void the_user_navigates_to_page(String string) {
         navigateModule(string);
     }
     @When("the user searches for {string} book")
-    public void the_user_searches_for_book(String string) {
-        search.sendKeys(string);
+    public void the_user_searches_for_book(String bookName) {
+        search.sendKeys(bookName);
+        this.classBookName = bookName;
     }
     @When("the user clicks edit book button")
     public void the_user_clicks_edit_book_button() {
-        editBook("Clean Code").click();
+        editBook(classBookName).click();
     }
     @Then("book information must match the Database")
     public void book_information_must_match_the_database() {
-        DB_Util.runQuery("select * from books where name='Clean Code';");
-        Map<String, String> rowMap = DB_Util.getRowMap(1);
-        Assert.assertEquals(bookName.getText(),rowMap.get("name"));
-        Assert.assertEquals(year.getText(),rowMap.get("year"));
-        Assert.assertEquals(isbn.getText(),rowMap.get("isbn"));
+        DB_Util.runQuery("select * from books where name='"+classBookName+"';");
+        Map<String, String> bookInfo = DB_Util.getRowMap(1);
+
+        String expectedBookName = bookInfo.get("name");
+        String expectedAuthorName =bookInfo.get("author");
+        String expectedISBN = bookInfo.get("isbn");
+        String expectedYear = bookInfo.get("year");
+        String expectedDesc = bookInfo.get("description");
+
+        String actualBookName = bookName.getAttribute("value");
+        String actualAuthorName = author.getAttribute("value");
+        String actualISBN = isbn.getAttribute("value");
+        String actualYear = year.getAttribute("value");
+        String actualDesc = description.getAttribute("value");
+
+
+
+        Assert.assertEquals(actualBookName,expectedBookName);
+        Assert.assertEquals(actualYear,expectedYear);
+        Assert.assertEquals(actualISBN,expectedISBN);
+        Assert.assertEquals(actualAuthorName,expectedAuthorName);
+        Assert.assertEquals(actualDesc,expectedDesc);
 
 
 
