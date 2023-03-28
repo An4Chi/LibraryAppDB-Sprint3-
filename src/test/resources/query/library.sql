56be21bdbe764fc4e9004c8342618a6b6086627b
An4Chi <kroliktha@gmail.com>
2023-03-28 08:16:02 -0500

    Added all queries for US

--- src/test/resources/query/library.sql
+++ src/test/resources/query/library.sql
@@ -33,5 +33,3 @@ select full_name,b.name,bb.borrowed_date from users u
                                                   inner join book_borrow bb on u.id = bb.user_id
                                                   inner join books b on bb.book_id = b.id
 where full_name='Test Student 1' and name='Head First Java' order by 3 desc;
-
-
