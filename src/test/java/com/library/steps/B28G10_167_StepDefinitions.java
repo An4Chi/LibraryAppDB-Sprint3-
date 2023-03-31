d552b358a2cd9ff176b03a30ba2d0209e13d1017
marick777 <marick777@gmail.com>
2023-03-30 22:23:38 -0500

    B28G10_167_StepDefinitions updated

--- src/test/java/com/library/steps/B28G10_167_StepDefinitions.java
+++ src/test/java/com/library/steps/B28G10_167_StepDefinitions.java
@@ -1,13 +1,12 @@
 package com.library.steps;
 
-import com.library.pages.BasePage;
 import com.library.pages.BookPage;
 import com.library.utility.BrowserUtil;
 import com.library.utility.DB_Util;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.support.ui.Select;
+
 
 import java.util.List;
 
@@ -20,8 +19,6 @@ public class B28G10_167_StepDefinitions extends BookPage {
         actualCategoryList=BrowserUtil.getAllSelectOptions(mainCategoryElement);
         actualCategoryList.remove(0);
         System.out.println("actualCategoryList = " + actualCategoryList);
-
-
 
     }
     @Then("verify book categories must match book_categories table from db")
