1cc976b7fb1e3809a6951d3ff6d586d291a54e81
AnnaCydeo <annatuk2502@gmail.com>
2023-04-01 20:35:50 -0500

    173

--- src/test/java/com/library/steps/B28G10_173_StepDefinitions.java
+++ src/test/java/com/library/steps/B28G10_173_StepDefinitions.java
@@ -8,27 +8,4 @@ import org.junit.Assert;
 
 public class B28G10_173_StepDefinitions {
 
-
-    @Given("establish the database connection")
-    public void establishTheDatabaseConnection() {
-
-    }
-
-    @When("Execute query to find most popular book genre")
-    public void executeQueryToFindMostPopularBookGenre() {
-        DB_Util.runQuery("select bc.name,count(*) from book_borrow bb\n" +
-                "                                 inner join books b on bb.book_id = b.id\n" +
-                "                                 inner join book_categories bc on b.book_category_id=bc.id\n" +
-                "group by name\n" +
-                "order by 2 desc;");
-
-    }
-
-
-    @Then("verify {string} is the most popular book genre")
-    public void verifyIsTheMostPopularBookGenre(String str) {
-        String actualResult = DB_Util.getFirstRowFirstColumn();
-        Assert.assertEquals(actualResult,str);
-
-    }
 }
--- src/test/resources/features/B28G10_173.feature
+++ src/test/resources/features/B28G10_173.feature
@@ -2,6 +2,6 @@ Feature: As a librarian, I want to know which genre of books is being borrowed t
 
   @db  @B28G10-186
 Scenario: Verify the common book genre that’s being borrowed.
-Given establish the database connection
-When Execute query to find most popular book genre
+    Given Establish the database connection
+    When I execute query to find most popular book genre
 Then verify "Fantasy" is the most popular book genre.
