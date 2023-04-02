5a2b324dda8295979cb95cd486caa1f1843a17ee
pahirdin <pahirdin1121@gmail.com>
2023-04-02 11:36:47 -0500

    created feature file

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = false,
-        tags = "@wip and @db and @ui and @B28G10-195"
+        tags = ""
 )
 public class CukesRunner {
 
